50c01bff0eb6528b516b8f1e0067aedf941b5f3f
Jan-Piet Mens <jpmens@gmail.com>
2015-08-22 13:59:55 +0200

    protect

--- ghash.h
+++ ghash.h
@@ -12,10 +12,9 @@
 # define FALSE 0
 #endif
 
+#ifdef HAVE_REDIS
 void redis_ping(redisContext **redis);
-// void ghash_store( redisContext **redis, char *ghash, char *addr, char *cc);
-// int ghash_cached( redisContext **redis, char *ghash, UT_string *addr, UT_string *cc);
-
 void last_storeredis(redisContext **redis, char *username, char *device, char *jsonstring);
+#endif
 int ghash_readcache(struct udata *ud, char *ghash, UT_string *addr, UT_string *cc);
 void ghash_storecache(struct udata *ud, JsonNode *geo, char *ghash, char *addr, char *cc);
