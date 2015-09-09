074434eb65d347e31d980a3da6d2ea7c236b5244
Jan-Piet Mens <jpmens@gmail.com>
2015-09-09 13:44:07 +0200

    http table: configurable column titles (e.g. language)

---
+++ wdocs/table/config.js
@@ -0,0 +1,14 @@
+config = {
+
+  /* Titles of columns in the table. May be "". */
+  column_titles: {
+	face:		"",
+  	topic:		"Topic",
+	fulldate:	"Date/Time",
+	name:		"Name",
+	user:		"User",
+	tid:		"TID",
+	cc:		"CC",
+	addr:		"Address"
+  }
+};
