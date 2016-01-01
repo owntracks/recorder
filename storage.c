865e986d3dbe8dd22409efb39b622c1e0a63860b
Jan-Piet Mens <jpmens@gmail.com>
2016-01-01 15:57:34 +0100

    update (C) date

--- storage.c
+++ storage.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Jan-Piet Mens <jpmens@gmail.com> and OwnTracks
+ * Copyright (C) 2015-2016 Jan-Piet Mens <jpmens@gmail.com> and OwnTracks
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
