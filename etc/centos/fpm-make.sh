30b93fe42f4335509efdd7c26b98247907752698
Jan-Piet Mens <jpmens@gmail.com>
2016-01-28 12:32:16 +0100

    package build Centos

---
+++ etc/centos/fpm-make.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+PACKAGEDIR=${PACKAGEDIR:=/tmp}
+
+tempdir=$(mktemp -d /tmp/ot-XXX)
+
+make install DESTDIR=$tempdir
+
+name="ot-recorder"
+version=$(awk '{print $NF;}' version.h | sed -e 's/"//g' )
+arch=$(uname -m)
+rpmfile="${PACKAGEDIR}/${name}_${version}_${arch}.rpm"
+
+rm -f "${rpmfile}"
+
+fpm -s dir \
+        -t rpm \
+        -n ${name} \
+        -v ${version} \
+        --vendor "OwnTracks.org" \
+        -a native \
+        --maintainer 'jpmens@gmail.com' \
+        --description "A lightweight back-end for consuming OwnTracks data from an MQTT broker" \
+        --license "https://github.com/owntracks/recorder/blob/master/LICENSE" \
+        --url "http://owntracks.org" \
+        -C $tempdir \
+        -p ${rpmfile} \
+        -d "libcurl" \
+        -d "libmosquitto1" \
+        -d "lua" \
+        --post-install etc/centos/postinst \
+        usr var
