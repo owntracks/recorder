f396c0f0e44f70ad6b41ef7ba04c385a61bb6c3f
Jan-Piet Mens <jpmens@gmail.com>
2016-02-10 07:52:16 +0100

    fix comment

--- contrib/faces/gravatar2card.sh
+++ contrib/faces/gravatar2card.sh
@@ -2,18 +2,18 @@
 # gravatar2card.sh (Feb 2017) by Jan-Piet Mens
 # Usage: gravatar2card email "full name"
 #
-# Calculate email hash and obtain a 40x40 PNG from Gravatar.
-# to BASE64. Create a JSON payload to be published to
-# owntracks/username/device/info
+# Calculate email hash and obtain a 40x40 PNG from Gravatar
+# and convert image to BASE64. Create a JSON payload to be published
+# retained to owntracks/username/device/info
 #
 # You probably want to do this:
 #
 # gravatar2card.sh email-address  "Jane Jolie" > card.json
-# mosquitto_pub -t owntracks/jane/phone -f card.json
+# mosquitto_pub -t owntracks/jane/phone -r -f card.json
 #
 # Note: the two commands cannot be piplelined (mosquitto_pub -l)
 # because of a bug in mosquitto_pub: https://bugs.eclipse.org/bugs/show_bug.cgi?id=478917
-# If you have a newish version it should work fine.
+# If you have a newer version it should work fine.
 
 
 [ $# -ne 2 ] && { echo "Usage: $0 email full-name" >&2; exit 2; }
