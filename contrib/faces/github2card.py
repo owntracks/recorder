e2b91665972e0bbf95cecf6075fac82629ec70d9
Jan-Piet Mens <jpmens@gmail.com>
2016-02-16 14:09:30 +0100

    GPL-2 license

--- contrib/faces/github2card.py
+++ contrib/faces/github2card.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-# github2card.py (Feb 2017) by Jan-Piet Mens
+
+# github2card.py
+# Copyright (C) 2016 Jan-Piet Mens <jpmens@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Usage: github2card username
 #
 # Read the Github profile of `username' and obtain user's
--- contrib/ot-ping.py
+++ contrib/ot-ping.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# OwnTracks Recorder
+# Copyright (C) 2015-2016 Jan-Piet Mens <jpmens@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # ot-ping.py
 # send a "pingping" to the OwnTracks Recorder via MQTT with "now"
 # in timestamp (tst). Then verify via Recorder's REST interface
@@ -16,7 +33,6 @@ import requests
 
 __author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
 __copyright__ = 'Copyright 2015 Jan-Piet Mens'
-__license__ = 'MIT'
 
 hostname    = 'localhost'
 port        = 1883
