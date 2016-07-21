f54cbb0de5504319c027461a5aad481c79626ccd
Jan-Piet Mens <jpmens@gmail.com>
2016-07-21 08:11:34 +0200

    Debian package dependent on libsodium13

--- etc/debian/fpm-make.sh
+++ etc/debian/fpm-make.sh
@@ -38,6 +38,7 @@ fpm -s dir \
         -d "libmosquitto1" \
         -d "liblua5.2-0" \
         -d "libconfig9" \
+        -d "libsodium13" \
 	--config-files etc/default/ot-recorder \
         --post-install etc/debian/postinst \
         usr var etc
