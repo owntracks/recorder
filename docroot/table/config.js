4b329d0d93f0c79b22e54ab4cc13288cdaaf1ec6
atjn <dev@atjn.dk>
2022-04-17 19:36:13 +0200

    Modernize live table

--- docroot/table/config.js
+++ docroot/table/config.js
@@ -1,17 +1,17 @@
-config = {
+export default {
 
   /* Titles of columns in the table. May be "". */
   column_titles: {
-	face:		"",
-  	topic:		"Topic",
-	fulldate:	"Date/Time",
-	name:		"Name",
-	user:		"User",
-	batt:		"Battery",
-	tid:		"TID",
-	cc:		"CC",
-	addr:		"Address",
-	vel:		"Vel",
-	cog:		"CoG"
-  }
+    face:		  "",
+    topic:    "Topic",
+    fulldate: "Date/Time",
+    name:		  "Name",
+    user:		  "User",
+    batt:		  "Battery",
+    tid:		  "TID",
+    cc:			  "CC",
+    addr:		  "Address",
+    vel:		  "Vel",
+    cog:		  "CoG",
+  },
 };
